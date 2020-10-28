39abec11bc8bd8ddcfaa38f2c70b7edc7c36d44b
Rizal Maulana Fahmi <gorenglahayam@gmail.com>
2020-10-29 06:50:48 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:dicoding_test/detail_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Wisata Bandung",
+      theme: ThemeData.dark(),
+      home: DetailScreen(),
+    );
+  }
+}
